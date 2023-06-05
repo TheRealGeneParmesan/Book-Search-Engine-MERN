acac897e64f1f3b7a33794d85ad5c6b271bd1dc4
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-06-05 14:48:09 -0700

    changed the server file again refresh err

--- server/server.js
+++ server/server.js
@@ -23,11 +23,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
